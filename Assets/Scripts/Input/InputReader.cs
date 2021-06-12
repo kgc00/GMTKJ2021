2f26115560015ba77494136418ce36cb83e7e498
kgc00 <kirbygc00@gmail.com>
2021-06-11 20:33:16 -0700

    movement and cam

--- Assets/Scripts/Input/InputReader.cs
+++ Assets/Scripts/Input/InputReader.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using Messages;
+using UniRx;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class InputReader : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace Input {
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Game/InputReader", order = 0)]
+    public class InputReader : ScriptableObject, Controls.IGameplayActions {
+        private Controls controls;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnEnable() {
+            controls ??= new Controls();
+            controls.Gameplay.SetCallbacks(this);
+            controls.Gameplay.Enable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context) {
+            var readValue = context.ReadValue<Vector2>();
+            if (context.performed) MessageBroker.Default.Publish(new MoveActionPerformed(readValue));
+            if (context.canceled) MessageBroker.Default.Publish(new MoveActionCanceled());
+        }
+
+        public void OnFire(InputAction.CallbackContext context) {
+            if(!context.performed) return;
+            
+            MessageBroker.Default.Publish(new FireAction());
+        }
     }
 }
---
+++ Assets/Scripts/Units/Movement.cs
@@ -0,0 +1,33 @@
+﻿using Input;
+using Messages;
+using UniRx;
+using UnityEngine;
+
+namespace Units {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Movement : MonoBehaviour {
+        [SerializeField] private InputReader _inputReader;
+        [SerializeField] private Rigidbody2D _rigidbody;
+        private Vector2 _motion;
+        [SerializeField, Range(1,20)] private float _speed = 5f;
+
+        private void OnEnable() {
+            MessageBroker.Default.Receive<FireAction>().Subscribe(_ => HandleFire());
+            MessageBroker.Default.Receive<MoveActionPerformed>().Subscribe(input => HandleMove(input.Motion));
+            MessageBroker.Default.Receive<MoveActionCanceled>().Subscribe(input => HandleMove(Vector2.zero));
+        }
+
+        private void HandleMove(Vector2 inputMotion) {
+            _motion = inputMotion;
+        }
+
+
+
+        private void HandleFire() { }
+
+        private void FixedUpdate() {
+            // _rigidbody.velocity += _motion * Time.fixedDeltaTime * _speed;
+            _rigidbody.MovePosition(_rigidbody.position + _motion * Time.fixedDeltaTime * _speed);
+        }
+    }
+}
