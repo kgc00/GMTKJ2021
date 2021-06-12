7d4d5ab7d2183550f2756e7dc193776b41d6fe01
momo1478 <monish1478@gmail.com>
2021-06-12 16:21:58 -0700

    Jenk iz happen, halp Kirb

--- Assets/Scripts/Mechanics/Anchor.cs
+++ Assets/Scripts/Mechanics/Anchor.cs
@@ -7,16 +7,19 @@ namespace Mechanics {
     public class Anchor : MonoBehaviour {
         [field: SerializeField] public Vector3 _throwTarget { get; private set; }
         [field: SerializeField] public Vector3 _deflectTarget { get; private set; }
+
         [SerializeField] private Rigidbody2D _rigidbody;
-        [SerializeField] private float _speed;
+        
         private AnchorState _state = AnchorState.Idle;
         private Transform _reelTransform;
 
         [Header("Common")] [SerializeField, Range(0, 3)]
         private float _accelFactor = 1;
 
+        [SerializeField, Range(5f, 50f)] private float _speed;
         [SerializeField, Range(1, 3)] private float _maxAccelFactor = 2;
         [SerializeField, Range(1, 10)] private float _damage;
+        [SerializeField, Range(0.01f, 1f)] private double _acceptanceThreshold = 0.1;
         [SerializeField] private bool deflectWithRelativeVelocity;
 
         [SerializeField, Range(0, 10), Header("Used when deflectWithRelativeVelocity is true")]
@@ -27,6 +30,8 @@ namespace Mechanics {
 
         private float _baseAccelFactor;
         Vector3 deflectTarget = Vector3.zero;
+
+        private Vector3 _previousVelocity;
 
         enum AnchorState {
             Idle,
@@ -41,45 +46,57 @@ namespace Mechanics {
             _baseAccelFactor = _accelFactor;
         }
 
-        private void Update() {
+        private void OnGUI()
+        {
+            GUILayout.Box(_state.ToString());
+            GUILayout.Box(_previousVelocity.magnitude.ToString());
+        }
 
+        private void Update() {
             if (_state == AnchorState.Thrown) {
-                if (Vector3.Distance(transform.position, _throwTarget) > 0.1) return;
+                if (Vector3.Distance(transform.position, _throwTarget) > _acceptanceThreshold) return;
                 _state = AnchorState.Landed;
             }
 
             if (_state == AnchorState.Reeling) {
                 if (_reelTransform == null) return;
 
-                if (Vector3.Distance(transform.position, _reelTransform.position) > 0.1) return;
+                if (Vector3.Distance(transform.position, _reelTransform.position) > _acceptanceThreshold/2f) return;
                 _state = AnchorState.Caught;
             }
 
             if (_state == AnchorState.Caught) { }
 
             if (_state == AnchorState.Deflected) {
-                if (Vector3.Distance(transform.position, _deflectTarget) > 0.1) return;
+                if (Vector3.Distance(transform.position, _deflectTarget) > _acceptanceThreshold) return;
                 _state = AnchorState.Landed;
             }
         }
 
         private void FixedUpdate() {
             if (_state == AnchorState.Thrown) {
-                var destination = _throwTarget - transform.position;
-                _rigidbody.MovePosition(transform.position + destination * Time.fixedDeltaTime * _speed);
+                var destination = (_throwTarget - transform.position).normalized;
+                Vector3 thrownVelocity = transform.position + destination * Time.fixedDeltaTime * _speed;
+                _rigidbody.MovePosition(thrownVelocity);
+                _previousVelocity = thrownVelocity;
             }
 
             if (_state == AnchorState.Reeling) {
                 if (_reelTransform == null) return;
                 
                 _accelFactor = Mathf.Min(_accelFactor + Time.fixedDeltaTime, _maxAccelFactor);
-                var destination = _reelTransform.position - transform.position;
+                var destination = (_reelTransform.position - transform.position).normalized;
                 _rigidbody.MovePosition(transform.position + destination * Time.fixedDeltaTime * _speed * _accelFactor);
             }
 
             if (_state == AnchorState.Deflected) {
-                var destination = deflectTarget - transform.position;
+                var destination = (deflectTarget - transform.position).normalized;
                 _rigidbody.MovePosition(transform.position + destination * Time.fixedDeltaTime * _speed);
+            }
+
+            if (_state == AnchorState.Landed) {
+                var destination = _previousVelocity;
+                _rigidbody.MovePosition(transform.position + (_previousVelocity * 0.25f));
             }
         }
 
@@ -103,6 +120,13 @@ namespace Mechanics {
         }
 
         private void OnCollisionEnter2D(Collision2D collision) {
+            HandleEnemyCollision(collision);
+
+            HandlePlayerCollision(collision);
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
             HandleEnemyCollision(collision);
 
             HandlePlayerCollision(collision);
